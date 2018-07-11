8b02081a2f824f1897c496d8b42875b412557d89
illusion_ <thortwo@outlook.com>
2018-07-11 16:33:01 +0800

    测试可正常运行

--- LogsParse/view.py
+++ LogsParse/view.py
@@ -7,17 +7,16 @@ import datetime
 
 
 def index(request):
-    context = dict()
-    context['hello'] = '老子吃火锅你吃火锅底料'
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
 
 
 def data(request):
     client = SSHClient()
     SSH = client.ssh_connect()
     spider_num, logs_num = client.spider_number(SSH, url='aidshe.com')
-    num = len(logs_num)/4
+    num = len(logs_num)
     data = {"baidu": spider_num[0:num], "shenma": spider_num[num:num*2], "s360": spider_num[num*2:num*3], "shougou": spider_num[num*3:num*4], "category": logs_num}
+    print(data)
     print('请求成功')
     SSH.close()
     return HttpResponse(json.dumps(data, ensure_ascii=False))
