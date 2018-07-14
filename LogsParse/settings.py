116681dc96509f0a181a5e21adc0c3a85892594a
illusion_ <thortwo@outlook.com>
2018-07-14 10:23:15 +0800

    添加新表

--- LogsParse/settings.py
+++ LogsParse/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = '3p5$jdqg*@yp6r&e8wa+*yh&ky#7*!qoe&9l-t%qt%-g!#-f94'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['23.88.37.88','127.0.0.1']
 
 
 # Application definition
