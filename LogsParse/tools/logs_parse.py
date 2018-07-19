4eaa4f25088dfaa90be307f66655b52334229dd4
illusion_ <thortwo@outlook.com>
2018-07-19 11:18:57 +0800

    path logs

--- LogsParse/tools/logs_parse.py
+++ LogsParse/tools/logs_parse.py
@@ -12,18 +12,18 @@ def insert(data, db):
     db.insert(sql, param)
 
 
-def read_logs(url, date):
-    path = os.path.abspath('.')
+def read_logs(_url, _date):
+    _path = os.path.abspath('.')
     try:
         spider = ['Baiduspider', '360Spider', 'sogou', 'Yisouspider']
         for x in range(0, 3):
-            order = 'cat /www/wwwroot/xbw/temp/robotlog/%s/%s |grep %s|wc -l' % (spider[x], date, url)
+            order = 'cat /www/wwwroot/xbw/temp/robotlog/%s/%s |grep %s|wc -l' % (spider[x], _date, _url)
             print(order)
             pi = Popen(order, shell=True, stdout=PIPE)
             result = pi.stdout.read()
-            cookie = open('%s/LogsParse/cookie/%s' % (path, url), 'a+')
-            cookie.write('%s %s %s' % (date.strip('.log'), url, result.strip('\n')))
-            print('%s %s %s' % (date, url, result))
+            cookie = open('%s/LogsParse/cookie/%s' % (_path, _url), 'a+')
+            cookie.write('%s %s %s' % (date.strip('.log'), _url, result.strip('\n')))
+            print('%s %s %s' % (_date, _url, result))
     except Exception as e:
         print(e)
 
@@ -34,15 +34,14 @@ if __name__ == "__main__":
         urls = []
         path = '/www/wwwroot/LogsParse_for_web/LogsParse/'
         dates = os.listdir('/www/wwwroot/xbw/temp/robotlog/Baiduspider/')
-        print(dates)
         group = os.listdir('/www/wwwroot/LogsParse_for_web/LogsParse/domain/')
-        print(group)
         for file in group:
             domain = open('%sdomain/%s/domain.txt' % (path, file), 'a+')
             for line in domain:
                 urls.append(line.strip('\n'))
         for url in urls:
             for date in dates:
+                print('%s %s' % (url, date))
                 read_logs(url, date)
     except Exception as e:
         print(e)
